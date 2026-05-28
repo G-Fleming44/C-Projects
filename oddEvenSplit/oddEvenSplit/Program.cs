c4c933d140830d8f5efee13db03dfb82c0d91a2d
George Fleming <George_fleming@Icloud.com>
2026-05-28 10:35:01 +0100

    oddEvenSplit

---
+++ oddEvenSplit/oddEvenSplit/Program.cs
@@ -0,0 +1,34 @@
+﻿
+using System.Diagnostics.Contracts;
+
+List<int> odd = new List<int>();
+List<int> even = new List<int>();
+
+
+for (int i = 0; i <= 20; i++)
+{
+
+    if (i % 2 == 0)
+    {
+        even.Add(i);
+    }
+    else 
+    {
+        odd.Add(i);
+    }
+}
+
+    Console.WriteLine("Now printing even numbers");
+
+    foreach (var item in even)
+    {
+        Console.Write($"{item} ");
+    }
+
+    Console.WriteLine(Environment.NewLine + "Now printing odd numbers");
+    foreach (var item in odd)
+    {
+        Console.Write($"{item} ");
+    }
+
+    Console.ReadLine();
