a210f00c0b32505012b51b2e14473dc096602ccc
George Fleming <George_fleming@Icloud.com>
2026-05-28 11:04:09 +0100

    Updated

---
+++ ArrayofMultiple/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics.Metrics;
+using System.Globalization;
+
+int num = 7;
+int length = 5;
+int[] result = new int[length];
+int counter = 0;
+
+
+for (int i = 1 ; i  <= result.Length; i++, counter++)
+{
+    result[counter] = num * i;
+
+}
+
+foreach (var nums in result)
+{
+    Console.WriteLine($"{nums} ");
+}
