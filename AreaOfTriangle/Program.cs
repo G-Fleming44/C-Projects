6cf49fe2d35e159c3c001af01b16b366eff19ed0
George Fleming <George_fleming@Icloud.com>
2026-05-28 13:21:31 +0100

    AreaOftriangle

---
+++ AreaOfTriangle/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+using System.Net.NetworkInformation;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+
+int width = ReadInt("Width");
+int height = ReadInt("height");
+
+Console.WriteLine($"The area is {AreaOfTriangle(width, height)}");
+
+
+static int AreaOfTriangle(int width, int height)
+{
+    return (width * height) / 2;
+}
+
+static int ReadInt(string message)
+{
+    Console.Write($"Enter {message}");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+
+
